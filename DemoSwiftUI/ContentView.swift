c6e5e019521bd30876ad890e9c316dadd311ed1d
Nikita Byzov <spamsmails@icloud.com>
2023-01-13 00:04:17 +0600

    добавлены элементы по SwiftUI

--- DemoSwiftUI/ContentView.swift
+++ DemoSwiftUI/ContentView.swift
@@ -6,83 +6,136 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    // тап-бар создаем
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        TabView {
+            FeedView()
+                .tabItem {
+                    Image(systemName: "note.text")
+                    Text("Feed")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "Plus")
-                    }
+            
+            PlayerView()
+                .tabItem {
+                    Image(systemName: "music.note.house")
+                    Text("Player")
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            VideoView()
+                .tabItem {
+                    Image(systemName: "music.note.tv.fill")
+                    Text("Video")
+                }
+            
+            RecorderView()
+                .tabItem {
+                    Image(systemName: "mic.fill")
+                    Text("Recorder")
+                }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct FeedView: View {
+    var body: some View {
+        VStack {
+            Text("Some text")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+            
+            Text("Another Text")
+                .fontWeight(.regular)
+        }
+    }
+}
+
+struct ProfileView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        
+        VStack(alignment: .center) {
+            
+            VStack(alignment: .center) {
+                Image("vkLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                // поднять к верху
+                // Spacer(minLength: 100)
+            }
+            // опустить вниз на
+            .padding(50)
+            
+            VStack(spacing: 0) {
+                TextField("Eemail or login", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button("Login") {
+                    let _ = print("Button Pressed with \(email) and \(password) values")
+                }
+                .frame(width: 350, height: 50)
+                .controlSize(.regular)
+                .buttonStyle(.borderedProminent)
+                .tint(Color(.systemBlue))
+                
+                .foregroundColor(.white)
+            }
+            .padding(20)
+        }
+    }
+}
+
+// код с лекции
+struct Universe: Identifiable {
+    @State var id: String
+    @State var name: String
+}
+
+struct PlayerView: View {
+    @State var universe: Universe?
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            
+            Button(action: { universe = .init(id: "1", name: "Marvel")}, label: { Text ("Marvel") })
+            
+            Button(action: { universe = .init(id: "2", name: "DC" )},
+                   label: { Text ("DC") })}
+        
+        .alert(item: $universe) { universe in
+            Alert(title: Text("Вы выбрали \(universe.name)"), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct VideoView: View {
+    var body: some View {
+        Color.gray
+    }
+}
+
+struct RecorderView: View {
+    var body: some View {
+        Color.gray
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
